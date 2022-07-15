2873e9fa01e84ef076d096f4d10640d81bedc182
kartikeya pandey <pandeykartikeya0604@gmail.com>
2022-07-15 11:26:17 +0530

    added modal

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import Cardscomp from "./Cardscomp";
-
+import { Modal, Button } from "react-bootstrap";
 
 export default function Home() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <>
       <section className="head h-50">
@@ -23,7 +28,7 @@ export default function Home() {
           </div>
           <div className="col-lg-4 col-xs-0 d-lg-block d-none">
             <div className="container">
-              <div className="d-flex justify-content-between flex-wrap">
+              <div className="d-flex justify-content-evenly flex-wrap">
                 <div className="dropdown mb-2">
                   <button
                     className="btn btn-secondary dropdown-toggle"
@@ -75,16 +80,80 @@ export default function Home() {
             </div>
           </div>
           <div
-            className="position-fixed d-flex justify-content-end "
+            className="position-fixed d-flex justify-content-end d-lg-none"
             style={{ bottom: "30px", right: "30px" }}
           >
             <button
               type="button"
               className="btn border-0 rounded-circle text-white p-3"
               style={{ backgroundColor: "crimson" }}
+              onClick={handleShow}
             >
               <i className="fa-solid fa-pen rounded-circle fs-5"></i>
             </button>
+            <Modal show={show} onHide={handleClose}>
+              <Modal.Header closeButton>
+                <Modal.Title>Lorem Ipsum</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+               <div className="container">
+              <div className="d-flex justify-content-evenly flex-wrap">
+                <div className="dropdown mb-2">
+                  <button
+                    className="btn btn-secondary dropdown-toggle"
+                    type="button"
+                    id="dropdownMenuButton1"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Write a Post
+                  </button>
+                  <ul
+                    className="dropdown-menu"
+                    aria-labelledby="dropdownMenuButton1"
+                  ></ul>
+                </div>
+                <button type="button" className="btn btn-primary mb-2">
+                  <i className="fa-solid fa-user-plus"></i> Join Group
+                </button>
+              </div>
+              <div className="input-group mb-3 mt-5 border-2 border-bottom">
+                <span
+                  className="input-group-text border-0 bg-white"
+                  id="basic-addon1"
+                >
+                  <i className="fa-solid fa-location-dot"></i>
+                </span>
+                <input
+                  type="text"
+                  className="form-control border-0"
+                  placeholder="Noida, India"
+                  aria-label="Username"
+                  aria-describedby="basic-addon1"
+                />
+                <span
+                  className="input-group-text border-0 bg-white"
+                  id="basic-addon1"
+                >
+                  <i className="fa-solid fa-pen"></i>
+                </span>
+              </div>
+              <div
+                className="alert alert-light text-secondary fs-6"
+                role="alert"
+              >
+                <i className="fa-solid fa-circle-exclamation me-1"></i> Your
+                location will help us serve better and extend a personalised
+                experience
+              </div>
+            </div>
+              </Modal.Body>
+              <Modal.Footer>
+                <Button variant="primary" onClick={handleClose}>
+                  Done
+                </Button>
+              </Modal.Footer>
+            </Modal>
           </div>
         </div>
       </div>
