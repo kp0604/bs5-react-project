c931228677e2ddfe05fff7316e08f61fb8b5d0c2
kartikeya pandey <pandeykartikeya0604@gmail.com>
2022-07-15 11:58:40 +0530

    added create account modal

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
+import { Modal, Button } from "react-bootstrap";
 
 export default function Header() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
     <nav className="navbar sticky-top navbar-light bg-light">
       <div className="container-fluid justify-content-around">
@@ -29,15 +34,74 @@ export default function Header() {
               class="btn btn-white dropdown-toggle ps-0"
               type="button"
               id="dropdownMenuButton1"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
+              onClick={handleShow}
             >
               <span className="text-primary fw-bold">Its Free!</span>
             </button>
-            <ul
-              class="dropdown-menu"
-              aria-labelledby="dropdownMenuButton1"
-            ></ul>
+            <Modal show={show} onHide={handleClose}>
+              <Modal.Header closeButton></Modal.Header>
+              <Modal.Body>
+                <div class="alert alert-success mb-2" role="alert">
+                  lets learn, share and inspire eachtother...
+                </div>
+                <div>
+                  <h3 class="mb-4">Create Account</h3>
+                  <div class="input-group">
+                    <input
+                      type="text"
+                      class="form-control bg-light"
+                      placeholder="Firstname"
+                    />
+                    <input
+                      type="text"
+                      class="form-control bg-light"
+                      placeholder="Lastname"
+                    />
+                  </div>
+                  <input
+                    type="text"
+                    class="form-control bg-light rounded-0"
+                    placeholder="Email"
+                    aria-describedby="basic-addon1"
+                  />
+                  <input
+                    type="text"
+                    class="form-control bg-light rounded-0"
+                    placeholder="Password"
+                    aria-describedby="basic-addon1"
+                  />
+                  <input
+                    type="text"
+                    class="form-control bg-light"
+                    placeholder="Confirm Password"
+                    aria-describedby="basic-addon1"
+                  />
+                </div>
+                <button
+                  class="btn btn-primary rounded-pill w-100 mt-2"
+                  onClick={handleClose}
+                >
+                  create account
+                </button>
+                <button
+                  class="btn btn-light border border-1 w-100 mt-4"
+                  onClick={handleClose}
+                >
+                  <i class="fa-brands fa-facebook me-2"></i>Signup with Facebook
+                </button>
+                <button
+                  class="btn btn-light border border-1 w-100 mt-2"
+                  onClick={handleClose}
+                >
+                  <i class="fa-brands fa-google me-2"></i>Signup with Google
+                </button>
+              </Modal.Body>
+              {/* <Modal.Footer>
+                <Button variant="primary" >
+                  Done
+                </Button>
+              </Modal.Footer> */}
+            </Modal>
           </div>
         </div>
       </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -96,57 +96,57 @@ export default function Home() {
                 <Modal.Title>Lorem Ipsum</Modal.Title>
               </Modal.Header>
               <Modal.Body>
-               <div className="container">
-              <div className="d-flex justify-content-evenly flex-wrap">
-                <div className="dropdown mb-2">
-                  <button
-                    className="btn btn-secondary dropdown-toggle"
-                    type="button"
-                    id="dropdownMenuButton1"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
+                <div className="container">
+                  <div className="d-flex justify-content-evenly flex-wrap">
+                    <div className="dropdown mb-2">
+                      <button
+                        className="btn btn-secondary dropdown-toggle"
+                        type="button"
+                        id="dropdownMenuButton1"
+                        data-bs-toggle="dropdown"
+                        aria-expanded="false"
+                      >
+                        Write a Post
+                      </button>
+                      <ul
+                        className="dropdown-menu"
+                        aria-labelledby="dropdownMenuButton1"
+                      ></ul>
+                    </div>
+                    <button type="button" className="btn btn-primary mb-2">
+                      <i className="fa-solid fa-user-plus"></i> Join Group
+                    </button>
+                  </div>
+                  <div className="input-group mb-3 mt-5 border-2 border-bottom">
+                    <span
+                      className="input-group-text border-0 bg-white"
+                      id="basic-addon1"
+                    >
+                      <i className="fa-solid fa-location-dot"></i>
+                    </span>
+                    <input
+                      type="text"
+                      className="form-control border-0"
+                      placeholder="Noida, India"
+                      aria-label="Username"
+                      aria-describedby="basic-addon1"
+                    />
+                    <span
+                      className="input-group-text border-0 bg-white"
+                      id="basic-addon1"
+                    >
+                      <i className="fa-solid fa-pen"></i>
+                    </span>
+                  </div>
+                  <div
+                    className="alert alert-light text-secondary fs-6"
+                    role="alert"
                   >
-                    Write a Post
-                  </button>
-                  <ul
-                    className="dropdown-menu"
-                    aria-labelledby="dropdownMenuButton1"
-                  ></ul>
+                    <i className="fa-solid fa-circle-exclamation me-1"></i> Your
+                    location will help us serve better and extend a personalised
+                    experience
+                  </div>
                 </div>
-                <button type="button" className="btn btn-primary mb-2">
-                  <i className="fa-solid fa-user-plus"></i> Join Group
-                </button>
-              </div>
-              <div className="input-group mb-3 mt-5 border-2 border-bottom">
-                <span
-                  className="input-group-text border-0 bg-white"
-                  id="basic-addon1"
-                >
-                  <i className="fa-solid fa-location-dot"></i>
-                </span>
-                <input
-                  type="text"
-                  className="form-control border-0"
-                  placeholder="Noida, India"
-                  aria-label="Username"
-                  aria-describedby="basic-addon1"
-                />
-                <span
-                  className="input-group-text border-0 bg-white"
-                  id="basic-addon1"
-                >
-                  <i className="fa-solid fa-pen"></i>
-                </span>
-              </div>
-              <div
-                className="alert alert-light text-secondary fs-6"
-                role="alert"
-              >
-                <i className="fa-solid fa-circle-exclamation me-1"></i> Your
-                location will help us serve better and extend a personalised
-                experience
-              </div>
-            </div>
               </Modal.Body>
               <Modal.Footer>
                 <Button variant="primary" onClick={handleClose}>
